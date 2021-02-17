e9999c37b19899d6bd1cad6d4bd6d0c93132e307
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-16 20:45:23 -0700

    Update App.js
    have to fix componentDidMount, throwing error right now

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import { getEvents, checkToken } from './api';
 
 // nneds to change to class or use this under useEffect?
 function App() {
-  async componentDidMount() {
-    const accessToken = localStorage.getItem("access_token");
-    const vaalidToken = accesssToken !== null ? await checkToken(accessToken) : false;
-    this.setState({ tokenCheck: validToken });
-    if(validToken === true) this.updateEvents();
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get("code");
+  // async componentDidMount() {
+  //   const accessToken = localStorage.getItem("access_token");
+  //   const vaalidToken = accesssToken !== null ? await checkToken(accessToken) : false;
+  //   this.setState({ tokenCheck: validToken });
+  //   if(validToken === true) this.updateEvents();
+  //   const searchParams = new URLSearchParams(window.location.search);
+  //   const code = searchParams.get("code");
 
-    this.mounted = true;
-    if (code && this.mounted === true && validToken === false) {
-      this.setState({tokenCheck: true});
-      this.updateEvents();
-    }
-  }
+  //   this.mounted = true;
+  //   if (code && this.mounted === true && validToken === false) {
+  //     this.setState({tokenCheck: true});
+  //     this.updateEvents();
+  //   }
+  // }
 
   return (
     <div className="App">
