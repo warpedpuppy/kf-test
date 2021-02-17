660f9ef63ba7839d2965183b160ee3a366c5bdcc
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-17 15:35:47 -0700

    Feature 2 Scenario 2.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import EventList from './EventList';
+import CitySearch from './CitySearch';
 
 class App extends Component {
 
@@ -8,6 +9,7 @@ class App extends Component {
     return(
       <div className='App'>
         <EventList />
+        <CitySearch />
       </div>
     )
   };
