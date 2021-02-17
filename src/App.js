c42d24fa474026593e78953597abe596be44cabd
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-17 11:41:58 -0700

    Revert "Google Verification Work"
    This reverts commit 1644805abcc9cdfe90fbc591693a1eb8a88e6e6a.

--- src/App.js
+++ src/App.js
@@ -4,48 +4,42 @@ import './App.css';
 import Login from './login-view/login';
 import { getEvents, checkToken } from './api';
 
-export default class App extends React.Component() {
+// nneds to change to class or use this under useEffect?
+function App() {
+  // async componentDidMount() {
+  //   const accessToken = localStorage.getItem("access_token");
+  //   const vaalidToken = accesssToken !== null ? await checkToken(accessToken) : false;
+  //   this.setState({ tokenCheck: validToken });
+  //   if(validToken === true) this.updateEvents();
+  //   const searchParams = new URLSearchParams(window.location.search);
+  //   const code = searchParams.get("code");
 
-  async componentDidMount() {
-    const accessToken = localStorage.getItem("access_token");
-    const validToken = accessToken !== null ? await checkToken(accessToken) : false;
-    this.setState({ tokenCheck: validToken });
-    if(validToken === true) this.updateEvents();
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get("code");
+  //   this.mounted = true;
+  //   if (code && this.mounted === true && validToken === false) {
+  //     this.setState({tokenCheck: true});
+  //     this.updateEvents();
+  //   }
+  // }
 
-    this.mounted = true;
-    if (code && this.mounted === true && validToken === false) {
-      this.setState({tokenCheck: true});
-      this.updateEvents();
-    }
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          Testing to see if code will change
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-  render() {
-    return (
-      // tokenCheck === false ? (
-      //   <div className="App">
-      //     <Login />
-      //   </div>
-      // ) : (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-      // )
-    );
-  }
-
-};
+export default App;
--- src/api.js
+++ src/api.js
@@ -1,33 +1,10 @@
 import axios from 'axios';
 
 // from the "Verify the Meet App" file
-export const checkToken = async (accessToken) => {
-    const result = await fetch (
-        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-        )
-        .then((res) => res.json())
-        .catch((error) => error.json());
 
-    return result.error ? false : true;
-    };
-
-const getAccessToken = async () => {
-    const accessToken = await localStorage.getItem('access_token');
-    const tokenCheck = accessToken && (await checkToken(accessToken));
-
-    if (!accessToken || !tokenCheck) {
-        await localStorage.removeItem('access_token');
-        const searchParams = new URLSearchParams(window.location.search);
-        const code = await searchParams.get('code');
-        if(!code) {
-        const results = await axios.get("https://xixfiq7och.execute-api.ca-central-1.amazonaws.com/dev/api/get-auth-url");
-        const { authURL } = results.data;
-        return(window.location.href = authURL);
-        }
-        return code && getToken(code);
-    }
-    return accessToken;
-    ;}
+const getToken = async (code) => {
+    removeQuery();
+};
 
 const removeQuery = () => {
     if (window.history.pushState && window.location.pathname) {
@@ -39,15 +16,34 @@ const removeQuery = () => {
     }
 };
 
-const getToken = async (code) => {
-    removeQuery();
-    const encodeCode = encodeURIComponent(code);
-    const { access_token } = await fetch(`https://xixfiq7och.execute-api.ca-central-1.amazonaws.com/dev/api/token/${encodeCode}`)
-        .then((res) => {
-            return res.json();
-        })
-        .catch((error) => error);
-        access_token && localStorage.setItem("access_token", access_token);
+const checkToken = async (accessToken) => {
+    const result = await fetch (
+        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+        )
+        .then((res) => res.json())
+        .catch((error) => error.json());
 
-        return access_token;
-};
+    return result.error ? false : true;
+    };
+
+const getAccessToken = async () => {
+    const accessToken = await
+    localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || !tokenCheck) {
+        await localStorage.removeItem('access_token');
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = await searchParams.get('code');
+        if(!code) {
+        const results = await axios.get(
+            'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.readonly&response_type=code&client_id=841717757727-88vckej4dd8ul7sd5u72c8fe47qb999i.apps.googleusercontent.com&redirect_uri=https%3A%2F%2FKSFlynn007.github.io%2Fmeet-app%2F'
+        );
+        const { authURL } = results.data;
+        return(window.location.href = authURL);
+        }
+        return code && getToken(code);
+    }
+    return accessToken;
+    ;}
+
