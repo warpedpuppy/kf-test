9014ffec99b1d2a8da6e4452007cca8a92daf21b
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-17 11:27:57 -0700

    commented out Google Ver work

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import { getEvents, checkToken } from './api';
 
 export default class App extends React.Component() {
 
-  async componentDidMount() {
-    const accessToken = localStorage.getItem("access_token");
-    const validToken = accessToken !== null ? await checkToken(accessToken) : false;
-    this.setState({ tokenCheck: validToken });
-    if(validToken === true) this.updateEvents();
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get("code");
+  // async componentDidMount() {
+  //   const accessToken = localStorage.getItem("access_token");
+  //   const validToken = accessToken !== null ? await checkToken(accessToken) : false;
+  //   this.setState({ tokenCheck: validToken });
+  //   if(validToken === true) this.updateEvents();
+  //   const searchParams = new URLSearchParams(window.location.search);
+  //   const code = searchParams.get("code");
 
-    this.mounted = true;
-    if (code && this.mounted === true && validToken === false) {
-      this.setState({tokenCheck: true});
-      this.updateEvents();
-    }
-  }
+  //   this.mounted = true;
+  //   if (code && this.mounted === true && validToken === false) {
+  //     this.setState({tokenCheck: true});
+  //     this.updateEvents();
+  //   }
+  // }
 
   render() {
     return (
