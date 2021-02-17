3668d71be09364aec6383e1edaa0b284705bfaca
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-17 14:55:22 -0700

    Feature 1 Scenario 1 Unit Test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
+import EventList from './EventList';
 
-class App extends Component() {
+class App extends Component {
 
   render() {
     return(
       <div className='App'>
-
+        <EventList />
       </div>
     )
   };
