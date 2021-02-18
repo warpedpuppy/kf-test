744d5d937358f23715c715be6df448b99d70f315
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-18 13:15:26 -0700

    App.js/App.test.js
    imported/rendered two new components and their tests, Event and NumberofEvents

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import Event from './Event';
+import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
 
@@ -10,6 +12,8 @@ class App extends Component {
       <div className='App'>
         <EventList />
         <CitySearch />
+        <Event />
+        <NumberOfEvents />
       </div>
     )
   };
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,6 +3,8 @@ import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
+import Event from '../Event';
+import NumberofEvents from '../NumberOfEvents';
 
 describe('<App /> component', () => {
     let AppWrapper;
@@ -17,4 +19,12 @@ describe('<App /> component', () => {
     test('render CitySearch', () => {
         expect(AppWrapper.find(CitySearch)).toHaveLength(1);
     });
+
+    test('render events', () => {
+        expect(AppWrapper.find(Event)).toHaveLength(1);
+    })
+
+    test('render number of events', () => {
+        expect(AppWrapper.find(NumberofEvents)).toHaveLength(1);
+    })
 })
