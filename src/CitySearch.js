a4f9844afa8c4ff710b684f06499c14b541ef68a
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-19 11:34:34 -0700

    CitySearch additional features
    hides search suggestions when option is clicked

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -4,7 +4,8 @@ class CitySearch extends Component {
 
   state = {
     query: '',
-    suggestions: []
+    suggestions: [],
+    showSuggestions: undefined
   }
 
   handleInputChanged = (event) => {
@@ -21,7 +22,8 @@ class CitySearch extends Component {
 
   handleItemClicked = (suggestion) => {
     this.setState({
-      query: suggestion
+      query: suggestion,
+      showSuggestions: false
     });
     this.props.updateEvents(suggestion);
   }
@@ -35,9 +37,11 @@ class CitySearch extends Component {
         className='city' 
         value={this.state.query} 
         onChange={this.handleInputChanged}
+        onFocus={() => { this.setState({ showSuggestions: true})}}
         />
 
-        <ul className="suggestions">
+{/* if showSuggestions is true, list is visable, style won't have display: none, so list won't become visible */}
+        <ul className="suggestions" style={this.state.showSuggestions ? {}: {display: 'none'}}>
           {this.state.suggestions.map((suggestion) => (
             <li 
             key={suggestion}
