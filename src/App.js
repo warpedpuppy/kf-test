c33e03d5fa7c1cfcdf974ccc0dc4ad1891534beb
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-22 14:01:55 -0700

    Update App.js
    fleshed out updateEvents function, passed updateEvents (prop) and numberOfEvents (state) to NumberOfEvents component to complete circle

--- src/App.js
+++ src/App.js
@@ -16,6 +16,35 @@ class App extends Component {
     currentLocation: 'all'
   }
 
+  updateEvents = (location, eventCount) => {
+    const {currentLocation, numberOfEvents} = this.state;
+    if (location) {
+      getEvents().then((events) => {
+        const locationEvents = 
+          location === 'all'
+            ? events
+            : events.filter((event) => event.location === location);
+        const filteredEvents = locationEvents.slice(0, numberOfEvents);
+        this.setState({
+          events: filteredEvents,
+          currentLocation: location
+        });
+      });
+    } else {
+      getEvents().then((events) => {
+        const locationEvents = 
+          currentLocation === 'all'
+            ? events
+            : events.filter((event) => event.location === currentLocation);
+        const filteredEvents = locationEvents.slice(0, eventCount);
+        this.setState({
+          events: filteredEvents,
+          numberOfEvents: eventCount
+        });
+      });
+    }
+  }
+
   componentDidMount() {
     this.mounted = true;
     getEvents().then((events) => {
@@ -29,22 +58,17 @@ class App extends Component {
     this.mounted = false;
   }
 
-  updateEvents = (location) => {
-    getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents
-      });
-    });
-  }
-
   render() {
     return(
       <div className='App'>
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
-        <NumberOfEvents />
+        <CitySearch 
+          locations={this.state.locations} 
+          updateEvents={this.updateEvents}
+          />
+        <NumberOfEvents 
+          numberOfEvents={this.state.numberOfEvents}
+          updateEvents={this.updateEvents}
+          />
         <EventList events={this.state.events}/>
       </div>
     )
