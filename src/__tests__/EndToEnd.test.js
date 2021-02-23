a5b651b66877148828fede06cdc486dad896f99e
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-23 13:53:34 -0700

    Puppeteer

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -4,9 +4,7 @@ import puppeteer from 'puppeteer';
 
 describe('show/hide an event details', () => {
     test('An event element is collapsed by default', async () => {
-    const browser = await puppeteer.launch({
-      executablePath: '/usr/bin/chromium-browser'
-    })
+    const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
     await page.goto('http://localhost:3000/');
