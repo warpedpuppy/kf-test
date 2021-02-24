e7b3bcce4f83efd414c9fd688a2cb0fc1d7619fb
KSFlynn007 <kelseyflynnn@gmail.com>
2021-02-24 10:08:33 -0700

    4.5 end to end testing configs

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,5 +1,7 @@
 
 import puppeteer from 'puppeteer';
+import Config from '../config';
+import { mockData } from '../mock-data';
  // no need for enzyme because the browser is what renders React components
 
 describe('show/hide an event details', () => {
@@ -8,12 +10,12 @@ describe('show/hide an event details', () => {
   beforeAll(async () => {
     jest.setTimeout(30000);
     browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 250,
-      ignoreDefaultArgs: ['--disable-extensions']
+      // headless: false,
+      // slowMo: 200,
+      ignoreDefaultArgs: ['--disable-extensions'],
     });
     page = await browser.newPage();
-    await page.goto('http://localhost:3000/')
+    await page.goto(Config.LOCAL_HOST)
     await page.waitForSelector('.Event');
   });
 
@@ -22,14 +24,11 @@ describe('show/hide an event details', () => {
   });
 
     test('An event element is collapsed by default', async () => {
-      await page.waitForSelector('.Event');
-
       const eventDetails = await page.$('.Event .event-expanded');
       expect(eventDetails).toBeNull();
   });
 
     test('User can expand an event to see its details', async () => {
-      await page.waitForSelector('.Event');
       await page.click('.Event .details-btn');
 
       const eventDetails = await page.$('.Event .event-expanded');
@@ -43,3 +42,37 @@ describe('show/hide an event details', () => {
   });
 
 })
+
+// describe('filter events by city', () => {
+//   let browser;
+//   let page;
+//   beforeAll(async () => {
+//     jest.setTimeout(30000);
+//     browser = await puppeteer.launch({
+//       // headless: false,
+//       // slowMo: 200,
+//       ignoreDefaultArgs: ['--disable-extensions']
+//     });
+//     page = await browser.newPage();
+//     await page.goto(Config.LOCAL_HOST)
+//     await page.waitForSelector('.Event');
+//   });
+
+//   afterAll(() => {
+//     browser.close();
+//   });
+
+//   test('When user has not searched for city, show upcoming events from all cities', async () => {
+//     const citySelector = await page.$('.Event');
+//     expect(citySelector).toHaveLength(mockData.length);
+//   });
+
+//   test('User should see list of suggestions when they search for a city', async () => {
+//     const citySelector = await page.$('.city');
+//     citySelector.click('input .city')
+//   })
+
+//   test('User can select a city from suggested list', async () => {
+//     const 
+//   })
+// })
