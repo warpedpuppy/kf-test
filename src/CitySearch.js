b5b9b789d8a3d1cd42f66fcd8ed9089aebb10521
KSFlynn007 <kelseyflynnn@gmail.com>
2021-03-02 11:00:09 -0700

    typo

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -25,7 +25,7 @@ class CitySearch extends Component {
       return this.setState({
         query:value,
         suggestions: [],
-        showSuggestions: false
+        showSuggestions: false,
         infoText: ''
       });
     }
