150b9aeecd17afca49b26ecd48b099bc38bd060a
KSFlynn007 <kelseyflynnn@gmail.com>
2021-03-02 16:56:25 -0700

    Update api.js
    offline

--- src/api.js
+++ src/api.js
@@ -18,6 +18,14 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if(!navigator.onLine) {
+    const events = localStorage.getItem('lastEvents');
+    NProgress.done();
+    return {
+      events: JSON.parse(events).events,
+    }
+  }
+
   const token = await getAccessToken();
 
   if (token) {
